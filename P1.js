d2ca38093ebcaf6fb0171a40ad9693bf0c665f3f
Martin <martinliu1993@hotmail.com>
2016-01-24 16:59:31 -0800

    Added stuff

--- P1.js
+++ P1.js
@@ -69,10 +69,26 @@ function makeCube() {
   return unitCube;
 }
 
-// GEOMETRY
+// GEOMETRY: Specifies the size
 var torsoGeometry = makeCube();
-var non_uniform_scale = new THREE.Matrix4().set(5,0,0,0, 0,5,0,0, 0,0,8,0, 0,0,0,1);
-torsoGeometry.applyMatrix(non_uniform_scale);
+var torsoScale = new THREE.Matrix4().set(5,0,0,0, 0,5,0,0, 0,0,8,0, 0,0,0,1);
+torsoGeometry.applyMatrix(torsoScale);
+
+var headGeometry = makeCube();
+var headScale = new THREE.Matrix4().set(3,0,0,0, 0,3,0,0, 0,0,2,0, 0,0,0,1);
+headGeometry.applyMatrix(headScale);
+
+var noseGeometry = makeCube();
+var noseScale = new THREE.Matrix4().set(1.5,0,0,0, 0,1.5,0,0, 0,0,1,0, 0,0,0,1);
+noseGeometry.applyMatrix(noseScale);
+
+var tailGeometry = makeCube();
+var tailScale = new THREE.Matrix4().set(1,0,0,0, 0,1,0,0, 0,0,5,0, 0,0,0,1);
+tailGeometry.applyMatrix(tailScale);
+
+var PawGeometry = makeCube();
+var PawScale = new THREE.Matrix4().set(3,0,0,0, 0,1,0,0, 0,0,5,0, 0,0,0,1);
+PawGeometry.applyMatrix(PawScale);
 
 // TO-DO: SPECIFY THE REST OF YOUR STAR-NOSE MOLE'S GEOMETRY. 
 // Note: You will be using transformation matrices to set the shape. 
@@ -83,8 +99,33 @@ torsoGeometry.applyMatrix(non_uniform_scale);
 
 
 
-// MATRICES
-var torsoMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,2.5, 0,0,1,0, 0,0,0,1);
+// MATRICES: Specifies the location
+var torsoMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,10, 0,0,1,0, 0,0,0,1);
+var headMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,0, 0,0,1,5, 0,0,0,1);
+var noseMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,0, 0,0,1,6, 0,0,0,1);
+var tailMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,0, 0,0,1,-4, 0,0,0,1);
+
+var frontRightPawMatrix = new THREE.Matrix4().set(1,0,0,-2.5, 0,1,0,-2.5, 0,0,1,4, 0,0,0,1);
+var frontLeftPawMatrix = new THREE.Matrix4().set(1,0,0,2.5, 0,1,0,-2.5, 0,0,1,4, 0,0,0,1);
+var backRightPawMatrix = new THREE.Matrix4().set(1,0,0,-2.5, 0,1,0,-3.5, 0,0,1,-2, 0,0,0,1);
+var backLeftPawMatrix = new THREE.Matrix4().set(1,0,0,2.5, 0,1,0,-3.5, 0,0,1,-2, 0,0,0,1);
+
+var headMatrixRelativeToTorso =  new THREE.Matrix4().multiplyMatrices(torsoMatrix, headMatrix);
+var noseMatrixRelativeToTorso =  new THREE.Matrix4().multiplyMatrices(torsoMatrix, noseMatrix);
+var tailMatrixRelativeToTorso =  new THREE.Matrix4().multiplyMatrices(torsoMatrix, tailMatrix);
+
+var rotate20Degrees = new THREE.Matrix4().set(  1,        0,         0,        0,
+                                        0, Math.cos(Math.PI/9),-Math.sin(Math.PI/9), 0,
+                                        0, Math.sin(Math.PI/9), Math.cos(Math.PI/9), 0,
+                                        0,        0,         0,        1);
+var frontRightPawMatrixRelativeToTorso = new THREE.Matrix4().multiplyMatrices(torsoMatrix, frontRightPawMatrix);
+frontRightPawMatrixRelativeToTorso.multiplyMatrices(frontRightPawMatrixRelativeToTorso, rotate20Degrees);
+var frontLeftPawMatrixRelativeToTorso = new THREE.Matrix4().multiplyMatrices(torsoMatrix, frontLeftPawMatrix);
+frontLeftPawMatrixRelativeToTorso.multiplyMatrices(frontLeftPawMatrixRelativeToTorso, rotate20Degrees);
+var backRightPawMatrixRelativeToTorso = new THREE.Matrix4().multiplyMatrices(torsoMatrix, backRightPawMatrix);
+backRightPawMatrixRelativeToTorso.multiplyMatrices(backRightPawMatrixRelativeToTorso, rotate20Degrees);
+var backLeftPawMatrixRelativeToTorso = new THREE.Matrix4().multiplyMatrices(torsoMatrix, backLeftPawMatrix);
+backLeftPawMatrixRelativeToTorso.multiplyMatrices(backLeftPawMatrixRelativeToTorso, rotate20Degrees);
 
 // TO-DO: INITIALIZE THE REST OF YOUR MATRICES 
 // Note: Use of parent attribute is not allowed.
@@ -97,6 +138,34 @@ var torsoMatrix = new THREE.Matrix4().set(1,0,0,0, 0,1,0,2.5, 0,0,1,0, 0,0,0,1);
 var torso = new THREE.Mesh(torsoGeometry,normalMaterial);
 torso.setMatrix(torsoMatrix)
 scene.add(torso);
+
+var head = new THREE.Mesh(headGeometry,normalMaterial);
+head.setMatrix(headMatrixRelativeToTorso)
+scene.add(head);
+
+var nose = new THREE.Mesh(noseGeometry,normalMaterial);
+nose.setMatrix(noseMatrixRelativeToTorso)
+scene.add(nose);
+
+var tail = new THREE.Mesh(tailGeometry,normalMaterial);
+tail.setMatrix(tailMatrixRelativeToTorso)
+scene.add(tail);
+
+var frontRightPaw = new THREE.Mesh(PawGeometry,normalMaterial);
+frontRightPaw.setMatrix(frontRightPawMatrixRelativeToTorso)
+scene.add(frontRightPaw);
+
+var frontLeftPaw = new THREE.Mesh(PawGeometry,normalMaterial);
+frontLeftPaw.setMatrix(frontLeftPawMatrixRelativeToTorso)
+scene.add(frontLeftPaw);
+
+var backRightPaw = new THREE.Mesh(PawGeometry,normalMaterial);
+backRightPaw.setMatrix(backRightPawMatrixRelativeToTorso)
+scene.add(backRightPaw);
+
+var backLeftPaw = new THREE.Mesh(PawGeometry,normalMaterial);
+backLeftPaw.setMatrix(backLeftPawMatrixRelativeToTorso)
+scene.add(backLeftPaw);
 
 // TO-DO: PUT TOGETHER THE REST OF YOUR STAR-NOSED MOLE AND ADD TO THE SCENE!
 // Hint: Hint: Add one piece of geometry at a time, then implement the motion for that part. 
@@ -137,24 +206,32 @@ function updateBody() {
   switch(true)
   {
       case(key == "U" && animate):
-      var time = clock.getElapsedTime(); // t seconds passed since the clock started.
+          var time = clock.getElapsedTime(); // t seconds passed since the clock started.
 
-      if (time > time_end){
-        p = p1;
-        animate = false;
-        break;
-      }
+          if (time > time_end){
+            p = p1;
+            animate = false;
+            break;
+          }
 
-      p = (p1 - p0)*((time-time_start)/time_length) + p0; // current frame 
+          p = (p1 - p0)*((time-time_start)/time_length) + p0; // current frame
 
-      var rotateZ = new THREE.Matrix4().set(1,        0,         0,        0, 
-                                            0, Math.cos(-p),-Math.sin(-p), 0, 
-                                            0, Math.sin(-p), Math.cos(-p), 0,
-                                            0,        0,         0,        1);
+          var rotateZ = new THREE.Matrix4().set(1,        0,         0,        0,
+                                                0, Math.cos(-p),-Math.sin(-p), 0,
+                                                0, Math.sin(-p), Math.cos(-p), 0,
+                                                0,        0,         0,        1);
 
-      var torsoRotMatrix = new THREE.Matrix4().multiplyMatrices(torsoMatrix,rotateZ);
-      torso.setMatrix(torsoRotMatrix); 
-      break
+          var torsoRotMatrix = new THREE.Matrix4().multiplyMatrices(torsoMatrix,rotateZ);
+          torso.setMatrix(torsoRotMatrix);
+
+          //Follows the rotation of torso. The head doesn't really rotate, it just keeps the constant distance from torso
+          var headRotMatrix = new THREE.Matrix4().multiplyMatrices(torsoRotMatrix, headMatrix);
+
+          //In addition to following the distance from the torso, the head also rotates around itself
+          //var headRotMatrix = new THREE.Matrix4().multiplyMatrices(headMatrix,rotateZ);
+          head.setMatrix(headRotMatrix);
+
+          break
 
       // TO-DO: IMPLEMENT JUMPCUT/ANIMATION FOR EACH KEY!
       // Note: Remember spacebar sets jumpcut/animate!
@@ -181,7 +258,7 @@ keyboard.domElement.addEventListener('keydown',function(event){
     camera.position.set(45,0,0);
     camera.lookAt(scene.position);}
   else if(keyboard.eventMatches(event,"U")){ 
-    (key == "U")? init_animation(p1,p0,time_length) : (init_animation(0,Math.PI/4,1), key = "U")}  
+    (key == "U")? init_animation(p1,p0,time_length) : (init_animation(0,Math.PI/4,1), key = "U")}
 
 
   // TO-DO: BIND KEYS TO YOUR JUMP CUTS AND ANIMATIONS
